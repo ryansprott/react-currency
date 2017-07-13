58bcb7743856fe1cbe500fa880d210d7b3d5194d
Ryan Sprott <rdsprott@nmsu.edu>
2017-07-13 12:34:04 -0600

    Added Bootstrap

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Container from './components/Container'
+import './node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Container />, document.getElementById('app'))
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -15,7 +15,11 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
-      {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']}
+      {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
+      {test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'}
     ]
   }
 };
